9efad9ec51a9356ba3437c9e7012d870a8c84a53
kiplimoboor <kiplimoarapboor@gmail.com>
2024-08-23 00:54:32 +0300

    utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -16,18 +16,16 @@ def validUTF8(data):
 
     for i in data:
         if count == 0:
-            if i >> 7 != 0b0:
-                return False
-
             if i >> 5 == 0b110 or i >> 5 == 0b1110:
                 count = 1
             elif i >> 4 == 0b1110:
                 count = 2
             elif i >> 3 == 0b11110:
                 count = 3
+            elif i >> 7 == 0b1:
+                return False
         else:
             if i >> 6 != 0b10:
                 return False
             count -= 1
-
     return count == 0
