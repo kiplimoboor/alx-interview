1f31f4cf8e4f1b0bc4288266eaf9698df6a89658
kiplimoboor <kiplimoarapboor@gmail.com>
2024-08-23 00:49:48 +0300

    utf-8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""A simple UTF 8 validator"""
+
+
+def validUTF8(data):
+    """
+    Checks if data input is a valid utf-8 entry
+    Args:
+        data: a list of integers
+    Return:
+        True if data is valid utf-8, False otherwise
+    """
+
+    count = 0
+
+    for i in data:
+        if count == 0:
+            if i >> 7 != 0b0:
+                return False
+
+            if i >> 5 == 0b110:
+                count = 1
+            elif i >> 4 == 0b1110:
+                count = 2
+            elif i >> 3 == 0b11110:
+                count = 3
+        else:
+            if i >> 6 != 0b10:
+                return False
+            count -= 1
+
+    return count == 0
