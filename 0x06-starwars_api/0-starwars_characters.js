3d3c540bd489993aeb5d4fefb4632956901e1924
kiplimoboor <kiplimoarapboor@gmail.com>
2024-09-04 13:39:49 +0300

    get film characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+/* eslint-disable space-before-function-paren */
+
+const request = require('request');
+
+const filmUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+
+const peopleUrl = 'https://swapi-api.alx-tools.com/api/people/';
+
+fetchData(peopleUrl, (people) => {
+  const filmChars = people.filter((person) => person.films.includes(filmUrl));
+  for (const character of filmChars) {
+    console.log(character.name);
+  }
+});
+
+function fetchData(url, callback, results = []) {
+  request(url, (err, res, body) => {
+    if (err) throw new Error(err);
+    if (res.statusCode !== 200) throw new Error('Non 200 status code');
+    const data = JSON.parse(body);
+    results.push(...data.results);
+    if (data.next) {
+      fetchData(data.next, callback, results);
+    } else {
+      callback(results);
+    }
+  });
+}
